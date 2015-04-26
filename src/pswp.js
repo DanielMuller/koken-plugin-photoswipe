6c9781e2e23ba21b39e7ce7ff77692860d67ea58
laklare <matt@nigdy.net>
2015-04-26 10:25:59 -0400

    Update pswp.js
    Videos don't play in the Photoswipe viewer so check to make sure the parent element is not of class "type_video" before opening Photoswipe. Videos do play within default Lightbox, so that should be used.

--- src/pswp.js
+++ src/pswp.js
@@ -265,7 +265,7 @@ var initPhotoSwipeFromDOM = function(options) {
 		}
 		if (!pswp_open) {
 			galleryElements.each(function(){
-				if ($(this).children("img").length>0) {
+				if ($(this).children("img").length>0 && !$(this).parent().hasClass('type_video')) {
 					$(this).click(function(e){
 						openPhotoSwipe(parseInt($(this).first().attr('data-pswp-gid')));
 						return false;
